e2c16bd1cfcdb8b50313a0e2df26cb2cc243c599
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2015-10-02 14:48:33 +0200

    Add createUser function

--- webclient/app/view1/view1.js
+++ webclient/app/view1/view1.js
@@ -9,6 +9,29 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope, $http) {
 
-}]);
+  $scope.createUser = function(user){
+
+    user = user || {
+      passportName: 'My passport name',
+      passportNr: 'My passport nr',
+      validToDate: (new Date()).toString(),
+      phone: '0767-703555',
+      email: 'gustaf.nk2@gmail.com',
+      citizenship: 'Sweden',
+      socialSecurityNr: '800101-7654'
+    };
+
+    return $http({
+      method: 'POST',
+      url: 'http://52.29.24.118/user/',
+      data: user
+    }).then(function successCallback(response) {
+      console.log("Success");
+    }, function errorCallback(response) {
+      console.log("Fail");
+    });
+  }
+
+});
