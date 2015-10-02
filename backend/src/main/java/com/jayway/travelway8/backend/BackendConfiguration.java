8fb69d06c654a8c8f99bd636b2dcb05e2556793a
Lars Östling <lars.ostling@jayway.com>
2015-10-02 11:09:22 +0200

    Add empty dropwizard project

---
+++ backend/src/main/java/com/jayway/travelway8/backend/BackendConfiguration.java
@@ -0,0 +1,10 @@
+package com.jayway.travelway8.backend;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class BackendConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
