d82ce85b70f53d87f688c240c5b35f6038d97126
Mattias Rosberg <mattias.rosberg@jayway.com>
2015-10-02 18:05:10 +0200

    Updated webview with java script support

--- android-client/TravelwayAdmin/app/src/main/java/app/travelway/jayway/com/travelwayadmin/WebFragment.java
+++ android-client/TravelwayAdmin/app/src/main/java/app/travelway/jayway/com/travelwayadmin/WebFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 /**
@@ -19,7 +20,16 @@ public class WebFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_web, container, false);
 
         WebView webView = (WebView) rootView.findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setSupportZoom(true);
+
         webView.loadUrl("http://52.29.24.118/#/register/nils.nilsson@travelway.com");
+
 
 
         return rootView;
